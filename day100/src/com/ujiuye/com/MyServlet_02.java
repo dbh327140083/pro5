c84c251bd85a9f2c27486aed2737b96159466498
dubaohua <327140083@qq.com>
2021-05-24 01:08:30 +0800

    idea中第一次提交

---
+++ day100/src/com/ujiuye/com/MyServlet_02.java
@@ -0,0 +1,7 @@
+package com.ujiuye.com;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+@WebServlet("/m2")
+public class MyServlet_02 extends HttpServlet {
+}
